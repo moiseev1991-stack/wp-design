3ebfd5e5f28c4f1a3dc9545ee24a0d85fd4ce7e6
moiseev1991-stack <moiseev1991-stack@users.noreply.github.com>
2026-04-26 18:21:08 +0000

    deploy: 43397ccafea20252156d8fdba747129453da3b31

---
+++ _next/static/chunks/app/kontakt/page-ce954efc05f59db6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[768,404,271,133,931,308],{226:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=226)}),_N_E=n.O()}]);
